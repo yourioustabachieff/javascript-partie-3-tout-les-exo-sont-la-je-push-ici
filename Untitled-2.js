2a705efa8da5df3781d3f815325f7e7e9f8f13c6
yourioustabachieff <yourioustabachieff@gmail.com>
2021-07-20 14:00:34 +0200

    exo 1 fini

--- Untitled-2.js
+++ Untitled-2.js
@@ -1,4 +1,14 @@
 
 
 
+function plusGros(){
+
+var myImg = document.getElementById("test")
+var grosseur = myImg.clientWidth;
+if (grosseur <= 600){
+myImg.style.width = grosseur + 20 + "px";
+
+   }
+}
+
 
