84f26bfb26f8cfb4fd2616b7ac8089abd0840436
yourioustabachieff <yourioustabachieff@gmail.com>
2021-07-23 12:14:59 +0200

    javascript partie 4 exo 2 distance reduite entres les lignes

--- Untitled-2.js
+++ Untitled-2.js
@@ -1,33 +1,18 @@
 
 var majeurOuMineur2 = document.getElementById("Sub");
 
-
-
-
 majeurOuMineur2.addEventListener("click", function(event){
-
-
   var majeurOuMineur = document.getElementById("sAge");
   var recup = majeurOuMineur.value;
   console.log("recup");
-
   if (recup >= 18) {
-
-
     document.getElementById("print").innerHTML="«vous êtes majeur.»";
     console.log("« vous êtes majeur»");
-  
   }
-  
-  
   else   {
-  
-  
     document.getElementById("print").innerHTML="«vous êtes mineur.»";
     console.log("« vous êtes mineur»");
-  
     }
-
 })
 
 
